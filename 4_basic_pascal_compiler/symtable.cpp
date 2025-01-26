1818ad06c13678afb299643093f9a13a9b1c140e
MRajczyk <mikolajrajczyk01@gmail.com>
2025-01-26 15:32:47 +0100

    add writing to file from buffer string stream to emitter

--- 4_basic_pascal_compiler/symtable.cpp
+++ 4_basic_pascal_compiler/symtable.cpp
@@ -1,4 +1,15 @@
 #include "global.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::stringstream out_string_stream;
 
 
+void export_code(std::string filename) {
+  std::ofstream out_file(filename);
+  out_file << out_string_stream.str();
+  out_file.close();
 
+  std::cout << out_string_stream.str() << std::endl;
+}
