56ec7423ebb09cbb6592a2003eea7f0ab75097e0
Mikołaj <mikolajrajczyk01@gmail.com>
2025-02-12 17:48:42 +0100

    oddany final boss

--- 5_advanced_pascal_compiler/vm, implemented compiler, test files/gcd.pas
+++ 5_advanced_pascal_compiler/vm, implemented compiler, test files/gcd.pas
@@ -11,7 +11,18 @@ begin
 end;
 
 
+function sub1(a, b: integer):integer;
+begin
+   sub1 := a - b
+end;
+
+function sub2(b, a: integer):integer;
+begin
+   sub2 := a - b
+end;
+
+
 begin
   read(x, y);
-  write(gcd(x, y))
+  write(gcd(sub1(24,6), sub2(1, 10)))
 end.
